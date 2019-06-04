c2c4b9b108b1a80fe906fb779f80ae084c9c4bf6
prince <getforprince@hotmail.com>
2019-06-04 19:37:38 +0530

    ui

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,47 +1,52 @@
-import React , { Component } from "react";
-import ImageModal from "./image.js";
-
+import React, { Component } from "react"
+import ImageModal from "./image.js"
 
 class Gallery extends Component {
-  constructor(props){
-    super(props);
-    this.state = { url: null , modal: 0 };
+  constructor(props) {
+    super(props)
+    this.state = { url: null, modal: 0 }
   }
 
-  componentWillMount(){
-    console.log(this.props.data.data.allPixabayPhoto.edges);
-  }
-
-  loadAllImages = (data)=>{
-    return data.map((res,i)=>{
-      // console.log(res.node.webformatURL);
+  loadAllImages = data => {
+    return data.map((res, i) => {
       return (
-        <div key={i} className="col-lg-3 col-md-4 col-xs-6 thumb" >
-          <a href="#" onClick={()=>{
-            this.setState({url: res.node.largeImageURL, modal: 1});
-          }}>
-            <img className="img-thumbnail"  src={res.node.webformatURL}/>
-          </a>
+        <div
+          key={i}
+          style={{ cursor: "pointer" }}
+          onClick={e => {
+            this.setState({ url: res.node.largeImageURL, modal: 1 })
+          }}
+          className="col-lg-3 col-md-4 col-xs-6 thumb"
+        >
+          <img className="img-thumbnail" src={res.node.largeImageURL} />
         </div>
-      );
+      )
     })
   }
 
-  toggle = () =>{
-    this.setState({modal: 0, url: null});
+  toggle = () => {
+    this.setState({ modal: 0, url: null })
   }
 
   render() {
     return (
-      <div className="container" style={{marginTop: "3vh"}}>
-        <div className="row">
-          {this.loadAllImages(this.props.data.data.allPixabayPhoto.edges)}
-          <ImageModal url={this.state.url} modal={this.state.modal} toggle={this.toggle}/>
+      <div
+        className="root"
+        style={{ backgroundColor: "crimson", paddingTop: "2vh" }}
+      >
+        <div className="container">
+          <div className="row">
+            {this.loadAllImages(this.props.data.data.allPixabayPhoto.edges)}
+            <ImageModal
+              url={this.state.url}
+              modal={this.state.modal}
+              toggle={this.toggle}
+            />
+          </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-
-export default Gallery;
+export default Gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,27 @@
-import React , { Component } from "react"
+import React, { Component } from "react"
 
-import "./../asserts/bootstrap.min.css";
+import "./../asserts/bootstrap.min.css"
 
 import Layout from "../components/layout"
 import Gallery from "../components/gallery"
 
+const IndexPage = data => <Gallery data={data} />
 
-const IndexPage = (data)=>(
-    <Gallery data={data}/>
-)
-
-export default IndexPage;
+export default IndexPage
 
 export const query = graphql`
   {
-allPixabayPhoto(limit: 30) {
-  edges {
-    node {
-      largeImageURL
-      pageURL
-      tags
-      user
-      webformatURL
-      id
+    allPixabayPhoto(limit: 30) {
+      edges {
+        node {
+          largeImageURL
+          pageURL
+          tags
+          user
+          webformatURL
+          id
+        }
+      }
     }
   }
-}
-}
 `
