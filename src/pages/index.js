7cd304638c40af5bfd57fb0afed155af95480abc
prince <getforprince@hotmail.com>
2019-06-04 16:13:52 +0530

    remove images array

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,6 @@ import "./../asserts/bootstrap.min.css";
 import Layout from "../components/layout"
 import Gallery from "../components/gallery"
 
-var arr = [
-  'https://picsum.photos/id/375/600/600',
-  'https://picsum.photos/id/741/600/600',
-  'https://picsum.photos/id/378/600/600',
-  'https://picsum.photos/id/194/600/600',
-  'https://picsum.photos/id/657/600/600',
-  'https://picsum.photos/id/181/600/600',
-  'https://picsum.photos/id/481/600/600',
-  'https://picsum.photos/id/481/600/600',
-  'https://picsum.photos/id/819/600/600',
-  'https://picsum.photos/id/663/600/600',
-  'https://picsum.photos/id/371/600/600',
-  'https://picsum.photos/id/375/600/600',
-  'https://picsum.photos/id/741/600/600',
-  'https://picsum.photos/id/378/600/600',
-  'https://picsum.photos/id/194/600/600',
-  'https://picsum.photos/id/657/600/600',
-  'https://picsum.photos/id/181/600/600',
-  'https://picsum.photos/id/481/600/600',
-  'https://picsum.photos/id/481/600/600',
-  'https://picsum.photos/id/819/600/600',
-  'https://picsum.photos/id/663/600/600',
-  'https://picsum.photos/id/371/600/600'
-];
 
 const IndexPage = (data)=>(
     <Gallery data={data}/>
